1a8711dcbfbeb7a3f0b65747d0631df6827fa7cc
gazi04 <gazmendhalili2016@gmail.com>
2025-09-04 13:56:11 +0200

    :recycle: Refactored the navbar from the layout
    This commit removes the navbar and puts it into a new Navbar Component
    for more readable code.

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,29 +1,14 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Navbar from './Navbar.vue'
 </script>
 
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
-    <div class="flex-1">
-      <a class="btn btn-ghost text-xl">daisyUI</a>
-    </div>
-    <div class="flex-none">
-      <ul class="menu menu-horizontal px-1">
-        <li><a>Link</a></li>
-        <li>
-          <details>
-            <summary>Parent</summary>
-            <ul class="bg-base-100 rounded-t-none p-2">
-              <li><a>Link 1</a></li>
-              <li><a>Link 2</a></li>
-            </ul>
-          </details>
-        </li>
-      </ul>
-    </div>
-  </div>
+ <Navbar />
 
-  <div class="m-4">
+  <div class="font-arimo">
     <RouterView />
   </div>
+
+  <!-- add footer later on -->
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar bg-base-100 shadow-sm">
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl font-pt">daisyUI</a>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
+        <li><a>Projects</a></li>
+        <li><a>Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
